4f355bfd0a8f059a1a7e858aca6f946514aa4ecc
Animesh Jha <jha.animesh01@gmail.com>
2022-12-20 17:40:52 +0530

    drqa build db

--- classical/Task1/build_tf_idf.py
+++ classical/Task1/build_tf_idf.py
@@ -12,13 +12,17 @@ import argparse
 import os
 import math
 import logging
-
+import regex
 from multiprocessing import Pool as ProcessPool
 from multiprocessing.util import Finalize
 from functools import partial
 from collections import Counter
-from . import utils
-from .tas1_interpretation2 import CoreNLPTokenizer
+import unicodedata
+from sklearn.utils import murmurhash3_32
+import sqlite3
+import copy 
+import pexpect
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -27,6 +31,370 @@ console = logging.StreamHandler()
 console.setFormatter(fmt)
 logger.addHandler(console)
 
+DEFAULTS = {
+    'corenlp_classpath': os.getenv('CLASSPATH')
+}
+
+
+def hash(token, num_buckets):
+    """Unsigned 32 bit murmurhash for feature hashing."""
+    return murmurhash3_32(token, positive=True) % num_buckets
+
+
+# ------------------------------------------------------------------------------
+# Text cleaning.
+# ------------------------------------------------------------------------------
+
+
+STOPWORDS = {
+    'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your',
+    'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she',
+    'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their',
+    'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that',
+    'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being',
+    'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an',
+    'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of',
+    'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through',
+    'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
+    'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then',
+    'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any',
+    'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor',
+    'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can',
+    'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've',
+    'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven',
+    'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren',
+    'won', 'wouldn', "'ll", "'re", "'ve", "n't", "'s", "'d", "'m", "''", "``"
+}
+
+
+
+def normalize(text):
+    """Resolve different type of unicode encodings."""
+    return unicodedata.normalize('NFD', text)
+
+
+def filter_word(text):
+    """Take out english stopwords, punctuation, and compound endings."""
+    text = normalize(text)
+    if regex.match(r'^\p{P}+$', text):
+        return True
+    if text.lower() in STOPWORDS:
+        return True
+    return False
+
+
+def filter_ngram(gram, mode='any'):
+    """Decide whether to keep or discard an n-gram.
+
+    Args:
+        gram: list of tokens (length N)
+        mode: Option to throw out ngram if
+          'any': any single token passes filter_word
+          'all': all tokens pass filter_word
+          'ends': book-ended by filterable tokens
+    """
+    filtered = [filter_word(w) for w in gram]
+    if mode == 'any':
+        return any(filtered)
+    elif mode == 'all':
+        return all(filtered)
+    elif mode == 'ends':
+        return filtered[0] or filtered[-1]
+    else:
+        raise ValueError('Invalid mode: %s' % mode)
+
+
+def save_sparse_csr(filename, matrix, metadata=None):
+    data = {
+        'data': matrix.data,
+        'indices': matrix.indices,
+        'indptr': matrix.indptr,
+        'shape': matrix.shape,
+        'metadata': metadata,
+    }
+    np.savez(filename, **data)
+
+
+
+class DocDB(object):
+    """Sqlite backed document storage.
+    Implements get_doc_text(doc_id).
+    """
+
+    def __init__(self, db_path):
+        self.path = db_path 
+        self.connection = sqlite3.connect(self.path, check_same_thread=False)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
+
+    def path(self):
+        """Return the path to the file that backs this database."""
+        return self.path
+
+    def close(self):
+        """Close the connection to the database."""
+        self.connection.close()
+
+    def get_doc_ids(self):
+        """Fetch all ids of docs stored in the db."""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT id FROM documents")
+        results = [r[0] for r in cursor.fetchall()]
+        cursor.close()
+        return results
+
+    def get_doc_text(self, doc_id):
+        """Fetch the raw text of the doc for 'doc_id'."""
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "SELECT text FROM documents WHERE id = ?",
+            (normalize(doc_id),)
+        )
+        result = cursor.fetchone()
+        cursor.close()
+        return result if result is None else result[0]
+
+
+
+
+class Tokens(object):
+    """A class to represent a list of tokenized text."""
+    TEXT = 0
+    TEXT_WS = 1
+    SPAN = 2
+    POS = 3
+    LEMMA = 4
+    NER = 5
+
+    def __init__(self, data, annotators, opts=None):
+        self.data = data
+        self.annotators = annotators
+        self.opts = opts or {}
+
+    def __len__(self):
+        """The number of tokens."""
+        return len(self.data)
+
+    def slice(self, i=None, j=None):
+        """Return a view of the list of tokens from [i, j)."""
+        new_tokens = copy.copy(self)
+        new_tokens.data = self.data[i: j]
+        return new_tokens
+
+    def untokenize(self):
+        """Returns the original text (with whitespace reinserted)."""
+        return ''.join([t[self.TEXT_WS] for t in self.data]).strip()
+
+    def words(self, uncased=False):
+        """Returns a list of the text of each token
+
+        Args:
+            uncased: lower cases text
+        """
+        if uncased:
+            return [t[self.TEXT].lower() for t in self.data]
+        else:
+            return [t[self.TEXT] for t in self.data]
+
+    def offsets(self):
+        """Returns a list of [start, end) character offsets of each token."""
+        return [t[self.SPAN] for t in self.data]
+
+    def pos(self):
+        """Returns a list of part-of-speech tags of each token.
+        Returns None if this annotation was not included.
+        """
+        if 'pos' not in self.annotators:
+            return None
+        return [t[self.POS] for t in self.data]
+
+    def lemmas(self):
+        """Returns a list of the lemmatized text of each token.
+        Returns None if this annotation was not included.
+        """
+        if 'lemma' not in self.annotators:
+            return None
+        return [t[self.LEMMA] for t in self.data]
+
+    def entities(self):
+        """Returns a list of named-entity-recognition tags of each token.
+        Returns None if this annotation was not included.
+        """
+        if 'ner' not in self.annotators:
+            return None
+        return [t[self.NER] for t in self.data]
+
+    def ngrams(self, n=1, uncased=False, filter_fn=None, as_strings=True):
+        """Returns a list of all ngrams from length 1 to n.
+
+        Args:
+            n: upper limit of ngram length
+            uncased: lower cases text
+            filter_fn: user function that takes in an ngram list and returns
+              True or False to keep or not keep the ngram
+            as_string: return the ngram as a string vs list
+        """
+        def _skip(gram):
+            if not filter_fn:
+                return False
+            return filter_fn(gram)
+
+        words = self.words(uncased)
+        ngrams = [(s, e + 1)
+                  for s in range(len(words))
+                  for e in range(s, min(s + n, len(words)))
+                  if not _skip(words[s:e + 1])]
+
+        # Concatenate into strings
+        if as_strings:
+            ngrams = ['{}'.format(' '.join(words[s:e])) for (s, e) in ngrams]
+
+        return ngrams
+
+    def entity_groups(self):
+        """Group consecutive entity tokens with the same NER tag."""
+        entities = self.entities()
+        if not entities:
+            return None
+        non_ent = self.opts.get('non_ent', 'O')
+        groups = []
+        idx = 0
+        while idx < len(entities):
+            ner_tag = entities[idx]
+            # Check for entity tag
+            if ner_tag != non_ent:
+                # Chomp the sequence
+                start = idx
+                while (idx < len(entities) and entities[idx] == ner_tag):
+                    idx += 1
+                groups.append((self.slice(start, idx).untokenize(), ner_tag))
+            else:
+                idx += 1
+        return groups
+
+
+class Tokenizer(object):
+    """Base tokenizer class.
+    Tokenizers implement tokenize, which should return a Tokens class.
+    """
+
+    def tokenize(self, text):
+        raise NotImplementedError
+
+    def shutdown(self):
+        pass
+
+    def __del__(self):
+        self.shutdown()
+
+
+class CoreNLPTokenizer(Tokenizer):
+
+    def __init__(self, **kwargs):
+        """
+        Args:
+            annotators: set that can include pos, lemma, and ner.
+            classpath: Path to the corenlp directory of jars
+            mem: Java heap memory
+        """
+        self.classpath = (kwargs.get('classpath') or
+                          DEFAULTS['corenlp_classpath'])
+        self.annotators = copy.deepcopy(kwargs.get('annotators', set()))
+        self.mem = kwargs.get('mem', '2g')
+        self._launch()
+
+    def _launch(self):
+        """Start the CoreNLP jar with pexpect."""
+        annotators = ['tokenize', 'ssplit']
+        if 'ner' in self.annotators:
+            annotators.extend(['pos', 'lemma', 'ner'])
+        elif 'lemma' in self.annotators:
+            annotators.extend(['pos', 'lemma'])
+        elif 'pos' in self.annotators:
+            annotators.extend(['pos'])
+        annotators = ','.join(annotators)
+        options = ','.join(['untokenizable=noneDelete',
+                            'invertible=true'])
+        cmd = ['java', '-mx' + self.mem, '-cp', '"%s"' % self.classpath,
+               'edu.stanford.nlp.pipeline.StanfordCoreNLP', '-annotators',
+               annotators, '-tokenize.options', options,
+               '-outputFormat', 'json', '-prettyPrint', 'false']
+
+        # We use pexpect to keep the subprocess alive and feed it commands.
+        # Because we don't want to get hit by the max terminal buffer size,
+        # we turn off canonical input processing to have unlimited bytes.
+        self.corenlp = pexpect.spawn('/bin/bash', maxread=100000, timeout=60)
+        self.corenlp.setecho(False)
+        self.corenlp.sendline('stty -icanon')
+        self.corenlp.sendline(' '.join(cmd))
+        self.corenlp.delaybeforesend = 0
+        self.corenlp.delayafterread = 0
+        self.corenlp.expect_exact('NLP>', searchwindowsize=100)
+
+    @staticmethod
+    def _convert(token):
+        if token == '-LRB-':
+            return '('
+        if token == '-RRB-':
+            return ')'
+        if token == '-LSB-':
+            return '['
+        if token == '-RSB-':
+            return ']'
+        if token == '-LCB-':
+            return '{'
+        if token == '-RCB-':
+            return '}'
+        return token
+
+    def tokenize(self, text):
+        # Since we're feeding text to the commandline, we're waiting on seeing
+        # the NLP> prompt. Hacky!
+        if 'NLP>' in text:
+            raise RuntimeError('Bad token (NLP>) in text!')
+
+        # Sending q will cause the process to quit -- manually override
+        if text.lower().strip() == 'q':
+            token = text.strip()
+            index = text.index(token)
+            data = [(token, text[index:], (index, index + 1), 'NN', 'q', 'O')]
+            return Tokens(data, self.annotators)
+
+        # Minor cleanup before tokenizing.
+        clean_text = text.replace('\n', ' ')
+
+        self.corenlp.sendline(clean_text.encode('utf-8'))
+        self.corenlp.expect_exact('NLP>', searchwindowsize=100)
+
+        # Skip to start of output (may have been stderr logging messages)
+        output = self.corenlp.before
+        start = output.find(b'{"sentences":')
+        output = json.loads(output[start:].decode('utf-8'))
+
+        data = []
+        tokens = [t for s in output['sentences'] for t in s['tokens']]
+        for i in range(len(tokens)):
+            # Get whitespace
+            start_ws = tokens[i]['characterOffsetBegin']
+            if i + 1 < len(tokens):
+                end_ws = tokens[i + 1]['characterOffsetBegin']
+            else:
+                end_ws = tokens[i]['characterOffsetEnd']
+
+            data.append((
+                self._convert(tokens[i]['word']),
+                text[start_ws: end_ws],
+                (tokens[i]['characterOffsetBegin'],
+                 tokens[i]['characterOffsetEnd']),
+                tokens[i].get('pos', None),
+                tokens[i].get('lemma', None),
+                tokens[i].get('ner', None)
+            ))
+        return Tokens(data, self.annotators)
 
 # ------------------------------------------------------------------------------
 # Multiprocessing functions
@@ -65,15 +433,15 @@ def count(ngram, hash_size, doc_id):
     global DOC2IDX
     row, col, data = [], [], []
     # Tokenize
-    tokens = tokenize(utils.normalize(fetch_text(doc_id)))
+    tokens = tokenize(normalize(fetch_text(doc_id)))
 
     # Get ngrams from tokens, with stopword/punctuation filtering.
     ngrams = tokens.ngrams(
-        n=ngram, uncased=True, filter_fn=utils.filter_ngram
+        n=ngram, uncased=True, filter_fn=filter_ngram
     )
 
     # Hash ngrams and count occurences
-    counts = Counter([utils.hash(gram, hash_size)
+    counts = Counter([hash(gram, hash_size)
                      for gram in ngrams])
 
     # Return in sparse matrix data format.
@@ -90,7 +458,7 @@ def get_count_matrix(args, db, db_opts):
     """
     # Map doc_ids to indexes
     global DOC2IDX
-    db_class = sqlite3
+    db_class = DocDB
     with db_class(**db_opts) as doc_db:
         doc_ids = doc_db.get_doc_ids()
     DOC2IDX = {doc_id: i for i, doc_id in enumerate(doc_ids)}
@@ -163,6 +531,8 @@ def get_doc_freqs(cnts):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('db_path', type=str, default=None,
+                        help='Path to sqlite db holding document texts')
     parser.add_argument('out_dir', type=str, default=None,
                         help='Directory for saving output files')
     parser.add_argument('--ngram', type=int, default=2,
@@ -198,4 +568,4 @@ if __name__ == '__main__':
         'ngram': args.ngram,
         'doc_dict': doc_dict
     }
-    utils.save_sparse_csr(filename, tfidf, metadata)
+    save_sparse_csr(filename, tfidf, metadata)
---
+++ classical/Task1/paragraphs_to_json.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df=pd.read_csv("data-dir/train_data.csv")
+df=df[['Paragraph']]
+df['id']=df.index.astype(str)
+
+df.rename({"Paragraph":"text"},axis=1,inplace=True)
+print(df.head())
+df.to_json("data-dir/paragraphs.json",orient="records",lines=True)
