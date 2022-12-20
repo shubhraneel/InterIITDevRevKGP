f6e2507a46cd402df546ce8251401b375e0db00b
Animesh Jha <jha.animesh01@gmail.com>
2022-12-20 19:02:30 +0530

    experiment

--- classical/Task1/paragraphs_to_json.py
+++ classical/Task1/paragraphs_to_json.py
@@ -7,3 +7,9 @@ df['id']=df.index.astype(str)
 df.rename({"Paragraph":"text"},axis=1,inplace=True)
 print(df.head())
 df.to_json("data-dir/paragraphs.json",orient="records",lines=True)
+
+df=pd.read_csv("data-dir/train_data.csv")
+df=df[['Question']]
+df['id']=df.index
+df.to_csv('data-dir/questions_only.csv')
+
