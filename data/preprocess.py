0b4dcb1aa57e47f380816c52e84b4844952e7037
shubhraneel <shubhraneelpal@gmail.com>
2022-12-21 20:51:22 +0530

    Synthetic QA - T5 - complete

---
+++ data/preprocess.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from ast import literal_eval
+
+def preprocess_fn(df):
+    """
+    No preprocessing in v1
+    """
+
+    data_dict = {}
+    data_dict["answers"] = []
+    data_dict["context"] = []
+    data_dict["id"] = []
+    data_dict["question"] = []
+    data_dict["title"] = []
+
+    # TODO: this is inefficient, we should tokenize everything at once
+    for i in range(len(df)):
+        answer_start = literal_eval(df.iloc[i]["Answer_start"])
+        answer_text = literal_eval(df.iloc[i]["Answer_text"])
+        context = df.iloc[i]["Paragraph"]
+        id = df.iloc[i]["Unnamed: 0"]
+        question = df.iloc[i]["Question"]
+        title = df.iloc[i]["Theme"]
+
+        answer = {"answer_start": answer_start, "text": answer_text}
+        data_dict["answers"].append(answer)
+
+        data_dict["context"].append(context)
+        data_dict["id"].append(id)
+        data_dict["question"].append(question)
+        data_dict["title"].append(title)
+
+    return data_dict
+    
--- synQA_t5.py
+++ synQA_t5.py
@@ -1,10 +1,16 @@
 import pytorch_lightning as pl
-from transformers import T5ForConditionalGeneration
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 import torch
 import numpy as np
+import pandas as pd
+from data.preprocess import preprocess_fn
+from data.dataloader import SQuAD_Dataset
+from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
 
-class QuestionGeneration(pl.LightningModule):
-    def __init__(self, config, train_dataloader = None, validation_dataloader = None, test_dataloader = None):
+class QuestionAnswerGeneration(pl.LightningModule):
+    def __init__(self):
+
         super().__init__()
         self.model = T5ForConditionalGeneration.from_pretrained("t5-base")
 
@@ -19,9 +25,9 @@ class QuestionGeneration(pl.LightningModule):
         out = self.model(
             input_ids=batch['paragraph_input_ids'],
             attention_mask=batch['paragraph_attention_mask'],
-            # decoder_input_ids=batch['question_input_ids'],
-            # decoder_attention_mask=batch['question_attention_mask'],
-            labels=batch['question_input_ids'],
+            decoder_input_ids=batch['question_input_ids'],
+            decoder_attention_mask=batch['question_attention_mask'],
+            # labels=batch['question_answer_input_ids'],
             output_hidden_states=True
         )
     
@@ -40,18 +46,37 @@ class QuestionGeneration(pl.LightningModule):
         return out.loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.config.training.lr)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-5)
         return optimizer
 
     def generate(self, test_dataloader):
         self.model.eval()
         outs = []
         for batch in test_dataloader:
-            outputs = self.model.generate(input_ids)
-            outs += outputs
-        return outs
+            outputs = self.model.generate(batch['paragraph_input_ids'], attention_mask = batch['paragraph_attention_mask'])
+            outs.append(outputs)
+        return torch.stack(outs)
 
+tokenizer = T5Tokenizer.from_pretrained('t5-base', TOKENIZERS_PARALLELISM=True, model_max_length=512, padding="max_length")
+df = pd.read_csv('data-dir/train_data.csv')
+df = preprocess_fn(df)
+df_train, df_test = train_test_split(df, test_size=0.2, random_state=3407)
+df_train, df_val = train_test_split(df_train, test_size=0.1, random_state=3407)
+
+train_ds = SQuAD_Dataset(df_train, tokenizer)
+val_ds = SQuAD_Dataset(df_val, tokenizer)
+test_ds = SQuAD_Dataset(df_test, tokenizer)
+
+train_dataloader = DataLoader(train_ds, batch_size=16, collate_fn=train_ds.collate_fn)
+val_dataloader = DataLoader(val_ds, batch_size=16, collate_fn=val_ds.collate_fn)
+test_dataloader = DataLoader(test_ds, batch_size=16, collate_fn=test_ds.collate_fn)
+
+model = QuestionAnswerGeneration()
 
 trainer = pl.Trainer()
-trainer.train()
+trainer.fit(model, train_dataloader, val_dataloader)
 
+generated_out = model.generate(test_dataloader)
+decoded_out = [tokenizer.decode(generated_out_item) for generated_out_item in generated_out]
+df_test['generated'] = decoded_out
+df_test.to_csv("generated_qa.csv")
