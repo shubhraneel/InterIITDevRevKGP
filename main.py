8d6909474df0dc660a3b01f104a75635427056e0
Varun Madhavan <varun@Muralis-MacBook-Pro.local>
2022-12-22 02:23:09 +0530

    Added fewshot_qa code

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from data import SQuAD_Dataset
 from src import AutoModel_Classifier_QA
 
 from pytorch_lightning.loggers import WandbLogger
+from src import AutoModel_Classifier_QA, FewShotQA_Model
 
 from torch.utils.data import DataLoader
 from sklearn.model_selection import GroupShuffleSplit
@@ -20,7 +21,7 @@ from sklearn.model_selection import GroupShuffleSplit
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--config', default="config.yaml", help="Config File")
+	parser.add_argument('--config', default="config-fewshot-qa.yaml", help="Config File")
 
 	args = parser.parse_args()
 	with open(args.config) as f:
@@ -51,17 +52,28 @@ if __name__ == "__main__":
 
 	df_train_alias = create_alias(df_train,config.data.alias_flag)
 
-	train_ds = SQuAD_Dataset(config, df_train_alias, tokenizer)
-	val_ds = SQuAD_Dataset(config, df_val, tokenizer)
-	test_ds = SQuAD_Dataset(config, df_test, tokenizer)
+	mask_token = tokenizer.mask_token
+
+	train_ds = SQuAD_Dataset(config, df_train_alias, tokenizer, mask_token)
+	val_ds = SQuAD_Dataset(config, df_val, tokenizer, mask_token)
+	test_ds = SQuAD_Dataset(config, df_test, tokenizer, mask_token)
 
 	train_dataloader = DataLoader(train_ds, batch_size=config.data.train_batch_size, collate_fn=train_ds.collate_fn)
 	val_dataloader = DataLoader(val_ds, batch_size=config.data.val_batch_size, collate_fn=val_ds.collate_fn)
 	test_dataloader = DataLoader(test_ds, batch_size=config.data.val_batch_size, collate_fn=test_ds.collate_fn)
 
-	model = AutoModel_Classifier_QA(config, tokenizer=tokenizer, logger=wandb_logger)
-	model.__train__(train_dataloader)
-	model.__inference__(test_dataloader)
-	classification_f1, qa_f1, ttime_per_example = model.calculate_metrics(test_dataloader)
+	if config.fewshot_qa:
+		model = FewShotQA_Model(config, tokenizer=tokenizer)
 
-	print(f"Classification F1: {classification_f1}, QA F1: {qa_f1}, Inference time per example: {ttime_per_example} ms")
+	else:
+		model = AutoModel_Classifier_QA(config, tokenizer=tokenizer, logger=wandb_logger)
+
+	model.__train__(train_dataloader)
+	# TODO: Uncomment this
+	# model.__inference__(test_dataloader)
+	
+	model.__evaluate__(val_dataloader)
+
+	# IMPORTANT TODO: fix this for fewshot_qa (this will currently give an error)
+	# classification_f1, qa_f1, ttime_per_example = model.calculate_metrics(test_dataloader)
+	# print(f"Classification F1: {classification_f1}, QA F1: {qa_f1}, Inference time per example: {ttime_per_example} ms")
--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,3 @@
 from .base_model import Base_Model
 from .model_v0 import AutoModel_Classifier_QA
+from .fewshot_qa_model import FewShotQA_Model
