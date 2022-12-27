82eb6496ceefde44c8d689e7b779b1653f8d426c
Varun Madhavan <varun.m.madhavan221313@gmail.com>
2022-12-27 22:45:56 +0530

    Rebasing branch varun, conflict fixes

--- data/dataloader.py
+++ data/dataloader.py
@@ -11,12 +11,11 @@ from torch.utils.data import DataLoader
 
 # TODO: memory optimization
 class SQuAD_Dataset(Dataset):
-	def __init__(self, config, df, tokenizer, train):
+	def __init__(self, config, df, tokenizer):
 		self.config = config
 
 		self.tokenizer = tokenizer
 		self.df = df.reset_index(drop=True)
-		self.train = train
 
 		# preprocess
 		self.data = preprocess_fn(self.df, self.tokenizer)
@@ -28,24 +27,22 @@ class SQuAD_Dataset(Dataset):
 						"title_input_ids", "title_attention_mask", "title_token_type_ids", 
 						"context_input_ids", "context_attention_mask", "context_token_type_ids",
 						"question_input_ids", "question_attention_mask", "question_token_type_ids",
-						"start_positions", "end_positions", "answerable", "question_context_offset_mapping"
+						"start_positions", "end_positions", "answerable", 
+						"question_context_offset_mapping"
 						]
 
 		for key in tokenized_keys:
 			self.data[key] = []
 
 		# TODO: Parallelise in batches
-		for idx in tqdm(range(0, len(self.data["question"]), 32)):
-			example = {key: self.data[key][idx:idx+32] for key in data_keys}
+		for idx in tqdm(range(0, len(self.data["question"]), self.config.data.tokenizer_batch_size)):
+			example = {key: self.data[key][idx:idx+self.config.data.tokenizer_batch_size] for key in data_keys}
 
-			if self.train:
-				tokenized_inputs = self._tokenize_train(example)
-			else:
-				tokenized_inputs = self._tokenize_test(example)
+			tokenized_inputs = self._tokenize(example)
 
 			for key in tokenized_keys:
 				self.data[key].extend(tokenized_inputs[key])
-
+			
 	def _get_theme_para_id_mapping(self):
 		"""
 		Get the indices of all paragraphs of a particular theme  
@@ -56,7 +53,7 @@ class SQuAD_Dataset(Dataset):
 		
 		return map_
 
-	def _tokenize_train(self, examples):
+	def _tokenize(self, examples):
 		# Some of the questions have lots of whitespace on the left, which is not useful and will make the
 		# truncation of the context fail (the tokenized question will take a lots of space). So we remove that
 		# left whitespace
@@ -81,6 +78,7 @@ class SQuAD_Dataset(Dataset):
 		# Since one example might give us several features if it has a long context, we need a map from a feature to
 		# its corresponding example. This key gives us just that.
 		sample_mapping = inputs.pop("overflow_to_sample_mapping")
+
 		# The offset mappings will give us a map from token to character position in the original context. This will
 		# help us compute the start_positions and end_positions.
 		offset_mapping = inputs["offset_mapping"]
@@ -201,62 +199,6 @@ class SQuAD_Dataset(Dataset):
 		# help us compute the start_positions and end_positions.
 		offset_mapping = inputs["offset_mapping"]
 		
-		# Let's label those examples!
-		inputs["start_positions"] = []
-		inputs["end_positions"] = []
-		inputs["answerable"] = []
-
-		for i, offsets in enumerate(offset_mapping):
-			# We will label impossible answers with the index of the CLS token.
-			input_ids = inputs["input_ids"][i]
-			# cls_index = (input_ids == 2).nonzero(as_tuple = True)[0]
-			cls_index = input_ids.index(self.tokenizer.cls_token_id)
-
-			# Grab the sequence corresponding to that example (to know what is the context and what is the question).
-			sequence_ids = inputs.sequence_ids(i)
-
-			# One example can give several spans, this is the index of the example containing this span of text.
-			sample_index = sample_mapping[i]
-			answers = examples["answers"][sample_index]
-			# If no answers are given, set the cls_index as answer.
-			if len(answers["answer_start"]) == 0:
-				inputs["start_positions"].append(cls_index)
-				inputs["end_positions"].append(cls_index)
-				inputs["answerable"].append(0)
-			else:
-				inputs["answerable"].append(1)
-				# Start/end character index of the answer in the text.
-				start_char = answers["answer_start"][0]
-				end_char = start_char + len(answers["text"][0])
-
-				# Start token index of the current span in the text.
-				token_start_index = 0
-				while sequence_ids[token_start_index] != (1 if self.config.data.pad_on_right else 0):
-					token_start_index += 1
-
-				# End token index of the current span in the text.
-				token_end_index = len(input_ids) - 1
-				while sequence_ids[token_end_index] != (1 if self.config.data.pad_on_right else 0):
-					token_end_index -= 1
-
-				# Detect if the answer is out of the span (in which case this feature is labeled with the CLS index).
-				if not (offsets[token_start_index][0] <= start_char and offsets[token_end_index][1] >= end_char):
-					inputs["start_positions"].append(cls_index)
-					inputs["end_positions"].append(cls_index)
-				else:
-					# Otherwise move the token_start_index and token_end_index to the two ends of the answer.
-					# Note: we could go after the last offset if the answer is the last word (edge case).
-					while token_start_index < len(offsets) and offsets[token_start_index][0] <= start_char:
-						token_start_index += 1
-					inputs["start_positions"].append(token_start_index - 1)
-					while offsets[token_end_index][1] >= end_char:
-						token_end_index -= 1
-					inputs["end_positions"].append(token_end_index + 1)
-
-		inputs["start_positions"] = torch.tensor(inputs["start_positions"])
-		inputs["end_positions"] = torch.tensor(inputs["end_positions"])
-		inputs["answerable"] = torch.tensor(inputs["answerable"])
-
 		# We keep the example_id that gave us this feature and we will store the offset mappings.
 		inputs["example_id"] = []
 
@@ -276,26 +218,29 @@ class SQuAD_Dataset(Dataset):
 				for k, o in enumerate(inputs["offset_mapping"][i])
 			]
 
-		inputs["question_context_input_ids"] = inputs.pop("input_ids")
-		inputs["question_context_attention_mask"] = inputs.pop("attention_mask")
-		inputs["question_context_token_type_ids"] = inputs.pop("token_type_ids")
-		# inputs["question_context_example_id"] = inputs.pop("example_id")
-		inputs["question_context_offset_mapping"] = inputs.pop("offset_mapping")
+		inputs["start_positions"] 					= torch.tensor(inputs["start_positions"])
+		inputs["end_positions"] 					= torch.tensor(inputs["end_positions"])
+		inputs["answerable"] 						= torch.tensor(inputs["answerable"])
 
-		title_tokenized = self.tokenizer(examples["title"], max_length=512, truncation="longest_first", return_offsets_mapping=True, padding="max_length", return_tensors="pt")
-		inputs["title_input_ids"] = title_tokenized["input_ids"]
-		inputs["title_attention_mask"] = title_tokenized["attention_mask"]
-		inputs["title_token_type_ids"] = title_tokenized["token_type_ids"]
+		inputs["question_context_input_ids"] 		= inputs.pop("input_ids")
+		inputs["question_context_attention_mask"] 	= inputs.pop("attention_mask")
+		inputs["question_context_token_type_ids"] 	= inputs.pop("token_type_ids")
+		inputs["question_context_offset_mapping"] 	= inputs.pop("offset_mapping")
 
-		context_tokenized = self.tokenizer(examples["context"], max_length=512, truncation="longest_first", return_offsets_mapping=True, padding="max_length", return_tensors="pt")    
-		inputs["context_input_ids"] = context_tokenized["input_ids"]
-		inputs["context_attention_mask"] = context_tokenized["attention_mask"]
-		inputs["context_token_type_ids"] = context_tokenized["token_type_ids"]
+		title_tokenized 							= self.tokenizer(examples["title"], max_length=512, truncation="longest_first", return_offsets_mapping=True, padding="max_length", return_tensors="pt")
+		inputs["title_input_ids"] 					= title_tokenized["input_ids"]
+		inputs["title_attention_mask"] 				= title_tokenized["attention_mask"]
+		inputs["title_token_type_ids"] 				= title_tokenized["token_type_ids"]
 
-		question_tokenized = self.tokenizer(examples["question"], max_length=512, truncation="longest_first", return_offsets_mapping=True, padding="max_length", return_tensors="pt")    
-		inputs["question_input_ids"] = question_tokenized["input_ids"]
-		inputs["question_attention_mask"] = question_tokenized["attention_mask"]
-		inputs["question_token_type_ids"] = question_tokenized["token_type_ids"]
+		context_tokenized							= self.tokenizer(examples["context"], max_length=512, truncation="longest_first", return_offsets_mapping=True, padding="max_length", return_tensors="pt")    
+		inputs["context_input_ids"] 				= context_tokenized["input_ids"]
+		inputs["context_attention_mask"] 			= context_tokenized["attention_mask"]
+		inputs["context_token_type_ids"] 			= context_tokenized["token_type_ids"]
+
+		question_tokenized 							= self.tokenizer(examples["question"], max_length=512, truncation="longest_first", return_offsets_mapping=True, padding="max_length", return_tensors="pt")    
+		inputs["question_input_ids"] 				= question_tokenized["input_ids"]
+		inputs["question_attention_mask"] 			= question_tokenized["attention_mask"]
+		inputs["question_token_type_ids"] 			= question_tokenized["token_type_ids"]
 
 		return inputs
 
@@ -306,7 +251,6 @@ class SQuAD_Dataset(Dataset):
 		return {key: self.data[key][idx] for key in self.data.keys()}
 
 	def collate_fn(self, items):
-
 		# batch = {key: torch.stack([x[key] for x in items], dim = 0).squeeze() for key in self.items.keys()}
 		# return batch
 
@@ -327,7 +271,7 @@ class SQuAD_Dataset(Dataset):
 			"question_context_input_ids":           torch.stack([torch.tensor(x["question_context_input_ids"]) for x in items], dim=0).squeeze(),
 			"question_context_attention_mask":      torch.stack([torch.tensor(x["question_context_attention_mask"]) for x in items], dim=0).squeeze(),
 			"question_context_token_type_ids":      torch.stack([torch.tensor(x["question_context_token_type_ids"]) for x in items], dim=0).squeeze(),
-			"question_context_offset_mapping":      torch.stack([torch.tensor(x["question_context_offset_mapping"]) for x in items], dim=0).squeeze(),
+			"question_context_offset_mapping":      [x["question_context_offset_mapping"] for x in items],
 
 			"answerable":                           torch.stack([x["answerable"] for x in items], dim=0),
 			"start_positions":                      torch.stack([x["start_positions"] for x in items], dim=0),
@@ -337,6 +281,33 @@ class SQuAD_Dataset(Dataset):
 			"question":								[x["question"] for x in items],
 			"context":								[x["context"] for x in items],
 			"id":									[x["id"] for x in items],
+			"answer":								[x["answers"]["text"] for x in items],
 		}
 
 		return batch
+	
+	def print_row(self, idx, return_dict=False):
+		example = self.__getitem__(idx)
+
+		print_dict = {}
+		print_dict["answer_text_gold"] = example["answers"]["text"]
+
+		context = example["context"]
+		offset_mapping = example["question_context_offset_mapping"]
+
+		start_index = example["start_positions"].item()
+		end_index = example["end_positions"].item()
+
+		decoded_answer = ""
+		if (start_index != 0 and end_index != 0):
+			start_char = offset_mapping[start_index][0]
+			end_char = offset_mapping[end_index][1]
+			
+			decoded_answer = context[start_char:end_char]
+
+		print_dict["answer_text_decoded"] = decoded_answer
+
+		if (not return_dict):
+			print(print_dict)
+		else:
+			return print_dict
--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import argparse
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 
+import torch
+from torch.utils.data import DataLoader
+
 from transformers import AutoTokenizer
+from pytorch_lightning.loggers import WandbLogger
 
 from config import Config
 from utils import set_seed
@@ -35,8 +39,9 @@ if __name__ == "__main__":
 
 	set_seed(config.seed)
 
-	# df = pd.read_excel(config.data.data_path)
 	df = pd.read_csv(config.data.data_path)
+	df = df.drop_duplicates(subset=["Unnamed: 0"])
+	# df = pd.read_excel(config.data.data_path)
 	# TODO: Split the dataset in a way where training theme question-context pair should not be
 	# split into train/test/val. Keep it only in the train.
 	# Mixup allowed between val and test.
@@ -50,8 +55,7 @@ if __name__ == "__main__":
  										random_state=config.seed)
 	#df_train, df_test = train_test_split(df, test_size=config.data.test_size, random_state=config.seed)
 	#df_train, df_val = train_test_split(df_train, test_size=config.data.test_size, random_state=config.seed)
-	
-	tokenizer = AutoTokenizer.from_pretrained(config.model.model_path, TOKENIZERS_PARALLELISM=True, model_max_length=512, padding="max_length") # add local_files_only=local_files_only if using server
+	tokenizer 			= AutoTokenizer.from_pretrained(config.model.model_path, TOKENIZERS_PARALLELISM=True, model_max_length=512, padding="max_length") # add local_files_only=local_files_only if using server
 
 	device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -77,16 +81,36 @@ if __name__ == "__main__":
 		print(f"QA F1: {qa_f1}, Inference time per example: {ttime_per_example} ms")
 	
 	else:
-		train_ds = SQuAD_Dataset(config, df_train_alias, tokenizer)
-		val_ds = SQuAD_Dataset(config, df_val, tokenizer)
-		test_ds = SQuAD_Dataset(config, df_test, tokenizer)
+		train_ds 			= SQuAD_Dataset(config, df_train, tokenizer)
+		print("length of train dataset: {}".format(train_ds.__len__()))
 
-		train_dataloader = DataLoader(train_ds, batch_size=config.data.train_batch_size, collate_fn=train_ds.collate_fn)
-		val_dataloader = DataLoader(val_ds, batch_size=config.data.val_batch_size, collate_fn=val_ds.collate_fn)
-		test_dataloader = DataLoader(test_ds, batch_size=config.data.val_batch_size, collate_fn=test_ds.collate_fn)
-		
+		val_ds 				= SQuAD_Dataset(config, df_val, tokenizer)
+		print("length of val dataset: {}".format(val_ds.__len__()))
+
+		test_ds 			= SQuAD_Dataset(config, df_test, tokenizer)
+		print("length of test dataset: {}".format(test_ds.__len__()))
+
+		# check if datasets have been encoded properly
+		# train_ds.print_row(15)
+		# val_ds.print_row(3)
+		# test_ds.print_row(69)
+		# example = train_ds.__getitem__(12)
+		# print(example["question_context_offset_mapping"])
+
+		train_dataloader 	= DataLoader(train_ds, batch_size=config.data.train_batch_size, collate_fn=train_ds.collate_fn)
+		val_dataloader 		= DataLoader(val_ds, batch_size=config.data.val_batch_size, collate_fn=val_ds.collate_fn)
+		test_dataloader 	= DataLoader(test_ds, batch_size=config.data.val_batch_size, collate_fn=test_ds.collate_fn)
+
+		device = "cuda" if torch.cuda.is_available() else "cpu"
+
 		model = BaselineQA(config, device).to(device)
-		model.to(device)
+		optimizer = torch.optim.Adam(model.parameters(), lr = config.training.lr)
+		trainer = Trainer(config, model, optimizer, device)
+
+		trainer.train(train_dataloader, val_dataloader)
+		# calculate_metrics(test_ds, test_dataloader, wandb_logger)
+		test_metrics = trainer.calculate_metrics(test_ds, test_dataloader)
+		print(test_metrics)
 
 		optimizer = torch.optim.Adam(model.parameters(), lr = config.training.lr)
 		trainer = Trainer(config, model, optimizer, device)
