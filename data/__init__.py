246878aeac729fec6a92a969dddf12cffa6e6800
shubhraneel <shubhraneelpal@gmail.com>
2023-01-04 16:48:35 +0000

    minor changes

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,4 @@
 from .dataloader import SQuAD_Dataset
 from .preprocess import preprocess_fn
 from .dataloader_fewshot import SQuAD_Dataset_fewshot
-from .dataloader_domain_discriminator import SQuAD_Dataset_title_label
+from .dataloader_domain_disc import SQuAD_Dataset_title_label
