b0de760d792e146750221d01fe25894cf70ed3f2
Ojasv Kamal <kamalojasv2000@gmail.com>
2023-01-18 17:10:09 +0530

    Corrected the indentation problem

--- main.py
+++ main.py
@@ -219,13 +219,13 @@ if __name__ == "__main__":
 
 			trainer.train(train_dataloader, val_dataloader)
 
-	if (config.save_model_optimizer):
-		print("saving model and optimizer at checkpoints/{}/model_optimizer.pt".format(config.load_path))
-		os.makedirs("checkpoints/{}/".format(config.load_path), exist_ok=True)
-		torch.save({
-		  'model_state_dict': model.state_dict(),
-		  'optimizer_state_dict': optimizer.state_dict(),
-		}, "checkpoints/{}/model_optimizer.pt".format(config.load_path))
+		if (config.save_model_optimizer):
+			print("saving model and optimizer at checkpoints/{}/model_optimizer.pt".format(config.load_path))
+			os.makedirs("checkpoints/{}/".format(config.load_path), exist_ok=True)
+			torch.save({
+			'model_state_dict': model.state_dict(),
+			'optimizer_state_dict': optimizer.state_dict(),
+			}, "checkpoints/{}/model_optimizer.pt".format(config.load_path))
 
 
 		if (config.inference):
