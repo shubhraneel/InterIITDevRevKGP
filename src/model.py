1f352f0613913b3990aa77d0fec0061ff38173e7
Nikhil Tudaha <nikhil.janya@gmail.com>
2023-01-27 16:42:03 +0530

    updated forward without end and start pos

--- src/model.py
+++ src/model.py
@@ -10,13 +10,14 @@ from transformers.onnx import FeaturesManager
 
 
 class BaselineQA(nn.Module):
-    def __init__(self, config, device):
+    def __init__(self, config, device, test=False):
         super(BaselineQA, self).__init__()
 
         self.config = config
         self.model = AutoModelForQuestionAnswering.from_pretrained(
             self.config.model.model_path
         )
+        self.test = test
         if config.model.two_step_loss:
             self.score = nn.Linear(config.model.dim, 1)
             self.loss_fct = nn.BCEWithLogitsLoss()
@@ -39,23 +40,15 @@ class BaselineQA(nn.Module):
         self.device = device
 
     def forward(self, batch):
+        input = dict()
+        input['input_ids'] = batch["question_context_input_ids"].to(self.device)
+        input['attention_mask'] = batch["question_context_input_ids"].to(self.device)
+        if not self.test:
+            input['start_positions'] = batch["start_positions"].to(self.device)
+            input['end_positions'] = batch["end_positions"].to(self.device)
         if not self.config.model.non_pooler:
-            out = self.model(
-                input_ids=batch["question_context_input_ids"].to(self.device),
-                attention_mask=batch["question_context_attention_mask"].to(self.device),
-                token_type_ids=batch["question_context_token_type_ids"].to(self.device),
-                start_positions=batch["start_positions"].to(self.device),
-                end_positions=batch["end_positions"].to(self.device),
-                output_hidden_states=True,
-            )
-        else:
-            out = self.model(
-                input_ids=batch["question_context_input_ids"].to(self.device),
-                attention_mask=batch["question_context_attention_mask"].to(self.device),
-                start_positions=batch["start_positions"].to(self.device),
-                end_positions=batch["end_positions"].to(self.device),
-                output_hidden_states=True,
-            )
+            input['token_type_ids'] = batch["question_context_token_type_ids"].to(self.device)
+        out = self.model(**input, output_hidden_states=True)
         if self.config.model.two_step_loss:
             cls_tokens = out.hidden_states[-1][:, 0]
             scores = self.score(cls_tokens)  # [32,1]
