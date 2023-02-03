b9dafed314e48a622b10891491d9f73f05909c77
Nikhil Tudaha <nikhil.janya@gmail.com>
2023-02-03 18:35:19 +0530

    updates bash file for corenlp

--- utils/drqa/install_corenlp.sh
+++ utils/drqa/install_corenlp.sh
@@ -8,7 +8,8 @@
 set -e
 
 # By default download to the data directory I guess
-read -p "Specify download path or enter to use default (data-dir/corenlp): " path
+# read -p "Specify download path or enter to use default (data-dir/corenlp): " path
+path=''
 DOWNLOAD_PATH="${path:-data-dir/corenlp}"
 echo $DOWNLOAD_PATH
 echo "Will download to: $DOWNLOAD_PATH"
@@ -25,15 +26,17 @@ mkdir -p "$DOWNLOAD_PATH"
 mv "/tmp/stanford-corenlp-full-2017-06-09/"*".jar" "$DOWNLOAD_PATH/"
 
 # Append to bashrc, instructions
-while read -p "Add to ~/.bashrc CLASSPATH (recommended)? [yes/no]: " choice; do
-    case "$choice" in
-        yes )
-            echo "export CLASSPATH=\$CLASSPATH:$DOWNLOAD_PATH/*" >> ~/.bashrc;
-            break ;;
-        no )
-            break ;;
-        * ) echo "Please answer yes or no." ;;
-    esac
-done
+# while read -p "Add to ~/.bashrc CLASSPATH (recommended)? [yes/no]: " choice; do
+#     case "$choice" in
+#         yes )
+#             echo "export CLASSPATH=\$CLASSPATH:$DOWNLOAD_PATH/*" >> ~/.bashrc;
+#             break ;;
+#         no )
+#             break ;;
+#         * ) echo "Please answer yes or no." ;;
+#     esac
+# done
+
+echo "export CLASSPATH=\$CLASSPATH:$DOWNLOAD_PATH/*" >> ~/.bashrc
 
 printf "\n*** NOW RUN: ***\n\nexport CLASSPATH=\$CLASSPATH:$DOWNLOAD_PATH/*\n\n****************\n"
